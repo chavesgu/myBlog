f9282151b30c861173a7e4e84962e812fb378927
guchunwen <595077937@qq.com>
2018-01-28 10:36:26 +0800

    chaves blog

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -5,7 +5,7 @@
       <router-link v-if="articleNum>0" v-for="n in articleNum"
                    :key="n" :to="{name:'article',params:{id:'c19930903'+n}}"
                    class="blogItem" :style="{'transition-delay':n*0.2+'s'}">
-        <div class="bg" :style="{background:`rgba(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},1)`}"></div>
+        <div class="bg" :style="{background:articleList[(n-1)%articleList.length].bgColor}"></div>
       </router-link>
     </transition-group>
   </div>
@@ -19,7 +19,13 @@
             showArticle:false,
             articleNum:0,
             articleList:[
-              {color:``}
+              {bgColor:`#72616e`},
+              {bgColor:`#e8846b`},
+              {bgColor:`#16528e`},
+              {bgColor:`#e54b4b`},
+              {bgColor:`#a2c5bf`},
+              {bgColor:`#167c80`},
+              {bgColor:`#0082c8`}
             ]
           }
       },
@@ -45,26 +51,31 @@
     bottom: 0;
     top: 0;
     .blogItem{
-      display: inline-block;
+      display: block;
       width: 320px;
       height: 100%;
       position: relative;
       .bg{
         position: absolute;
         left: 0;
-        right: 0;
-        margin: auto;
-        bottom: 0;
+        top: 0;
         width: 100%;
         height: 100%;
+        z-index: 1;
+        transition: box-shadow .6s;
+        &:hover{
+          z-index: 5;
+          box-shadow: 0 0 30px 5px rgba(0,0,0,0.7);
+          transition: box-shadow .6s;
+        }
       }
     }
   }
 }
-  .blogList-enter-active,.blogList-leave-active{
+  .blogList-enter-active{
     transition: transform .5s;
   }
-  .blogList-enter,.blogList-leave-to{
+  .blogList-enter{
     transform: translateY(100vh);
   }
 
